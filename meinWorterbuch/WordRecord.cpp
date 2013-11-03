a6ce3c2ec87643c447229b6a7a65b19955095ac9
Volkan SALMA <volkansalma@yahoo.com>
2013-11-03 23:07:09 +0200

    first commit. Db connection is ok. It is compilable on windows and linux machine now. There are three sample words in dictionary.

---
+++ meinWorterbuch/WordRecord.cpp
@@ -0,0 +1,16 @@
+#include "WordRecord.h"
+
+
+WordRecord::WordRecord()
+{
+    this->isSuccefullySelected = 0;
+    this->artikel = 0;
+    this->suffix = 0;
+    this->word = "";
+    this->meaning = "";
+}
+
+WordRecord::~WordRecord()
+{
+
+}
